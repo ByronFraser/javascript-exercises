bee78b1292c2e36ba63926c0459a2b971350fff3
ByronFraser <byronfraser@live.co.za>
2024-12-24 14:16:36 +0700

    factorial_finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1+num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1-num2)
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let total = 0;
+	for (let i = 0; i < nums.length; i++){
+    total += nums[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+let total = 1;
+for (let i = 0; i < nums.length; ++i){
+  total *= nums[i];
+}
+return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (Math.pow(num1, num2))
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let total = 1;
+	if (num === 0){
+    return (1);
+  } else{
+    for (let i = 1; i<=num; i++){
+      total *= i;
+    }
+    
+  }
+  return (total);
+}
 
 // Do not edit below this line
 module.exports = {
