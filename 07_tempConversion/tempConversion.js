eb9ccbc778d27b5c9b24cf58964867a64f7b5020
ByronFraser <byronfraser@live.co.za>
2024-12-24 13:11:16 +0700

    temp_conversion_finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  
+  let temp = Math.round(10*((fTemp - 32) * (5/9))) / 10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let temp = Math.round(10*((cTemp * 9/5) + (32))) / 10;
+  return temp;
 };
 
 // Do not edit below this line
