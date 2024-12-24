1817b21a174f90fa3c4aa8dbb2c1879987e32c47
ByronFraser <byronfraser@live.co.za>
2024-12-24 12:33:09 +0700

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstNum,lastNum) {
 
+    let sum = 0;
+    let num1 = 0;
+    let num2 = 0;
+    if (typeof firstNum !== 'number' || typeof lastNum !== 'number'){
+        return ('ERROR');
+    } else if (firstNum <0 || lastNum <0){
+        return ('ERROR');
+    } else if (firstNum - Math.floor(firstNum) !== 0){
+        return ('ERROR');
+    } else if (firstNum < lastNum){
+        num1 = firstNum;
+        num2 = lastNum;
+    } else if (lastNum < firstNum){
+        num1 = lastNum;
+        num2 = firstNum;
+    } else {
+        return ('ERROR');
+    }
+    for (let i = num1; i <= num2; i++){
+        sum = sum + i;
+    }
+    
+
+
+    return (sum);
 };
 
 // Do not edit below this line
