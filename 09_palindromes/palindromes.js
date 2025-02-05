2a3b7c2a0773c879cd619794240394472cc853f2
ByronFraser <byronfraser@live.co.za>
2025-02-05 10:30:44 +0700

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+
+let arrPalindrome = palindrome.toLowerCase().split("");
+
+let arrRemoveExtras = arrPalindrome.filter(char => /[a-z0-9]/.test(char));
+
+
+let arrReversed = arrRemoveExtras.slice().reverse();
+
+if (arrReversed.length === arrRemoveExtras.length) {
+    for (let i = 0; i < arrRemoveExtras.length; i++){
+        if (arrRemoveExtras[i] != arrReversed[i]) {
+            return false
+        }
+             
+    }
+    return true;
+} else
+return false;
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
