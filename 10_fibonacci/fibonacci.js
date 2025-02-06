efbf39835dc3e560937bc6a10f87f0aafa47af0e
ByronFraser <byronfraser@live.co.za>
2025-02-06 10:17:18 +0700

    Finished_2

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
 
+
+const fibonacci = function(num) {
+    
+    let arrNumbers = [0, 1];
+    number = Number(num);
+    if (number<0) return 'OOPS';
+
+    for (let i = 0; i < number; i++){
+        arrNumbers.push(arrNumbers[i]+arrNumbers[i+1])
+    }
+    console.log(arrNumbers);
+    return arrNumbers[number]
 };
-
+fibonacci();
 // Do not edit below this line
 module.exports = fibonacci;
