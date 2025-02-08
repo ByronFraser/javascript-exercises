f990c79095746de62114968dcdb48dc439e6630e
ByronFraser <byronfraser@live.co.za>
2025-02-08 09:55:26 +0700

    finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let arr = [];
 
+     return books.map(book => book.title);
+
+return arr;
 };
 
 // Do not edit below this line
