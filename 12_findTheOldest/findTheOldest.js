45fe88b9d4b3b5a3b01a65dd380a187df174f377
ByronFraser <byronfraser@live.co.za>
2025-02-09 13:29:15 +0700

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    });
 };
+
+module.exports = findTheOldest;
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
